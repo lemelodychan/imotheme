2251a8fafc7e269c25ef23de99edfc91c2a53815
Melody <elodie.dassance@gmail.com>
2024-02-07 15:53:43 +0900

    Update qeel-drawer.js

--- v0/js/qeel-drawer.js
+++ v0/js/qeel-drawer.js
@@ -119,7 +119,7 @@ $(document).ready(function () {
                             var secondUrl = 'https://imolab.forumotion.asia' + '/u' + userId;
                             console.log(fieldContent);
 
-                            if(fieldContent === "0" || fieldContent === 0) {
+                            if(userId === "0" || userId === 0) {
                                 $.ajax({
                                     url: secondUrl,
                                     method: 'GET',
