fe692dace514aa22d292bcfed6f987dd1f82a7c2
Melody <elodie.dassance@gmail.com>
2024-03-15 07:20:36 +0100

    Create registre-list.js

---
+++ v0/js/registre-list.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    const supabaseUrl = 'https://gbejdguvqhsdvwtmqfbp.supabase.co';
+    const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdiZWpkZ3V2cWhzZHZ3dG1xZmJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0NjUxOTksImV4cCI6MjAyNjA0MTE5OX0.kK8RMRPvXehp86KIM-sXfq9qERgaSLEFOqXuwZVZUqw';
+    const { createClient } = supabase;
+    const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
+
+    async function fetchAndDisplayItems() {
+        try {
+            const { data, error } = await supabase
+                .from('Registre') 
+                .select('*'); 
+            if (error) throw error;
+
+            $('#itemList').empty();
+            data.forEach(item => {
+                const listItem = `<li>ID: ${item.id}, Nom: ${item.nom}, Prenom: ${item.prenom}, Surnom: ${item.surnom}, NDF: ${item.ndf}</li>`;
+                $('#itemList').append(listItem);
+            });
+          
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+    fetchAndDisplayItems();
+  
+});
